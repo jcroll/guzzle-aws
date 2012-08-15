9aaab1ebff498aded0c20c9371f4ec0b96a52555
Ryan J. Geyer <me@ryangeyer.com>
2012-08-15 11:52:26 -0700

    Added the empty string case

--- Signature/SignatureV2.php
+++ Signature/SignatureV2.php
@@ -60,7 +60,7 @@ class SignatureV2 extends AbstractSignature
         uksort($request, $options['sort_method']);
 
         foreach ($request as $k => $v) {
-            if (isset($k) && isset($v) && !in_array($k, $options['ignore'])) {
+            if (isset($k) && isset($v) && (is_bool($v) || strval($v) != '') && !in_array($k, $options['ignore'])) {
                 if ($parameterString) {
                     $parameterString .= '&';
                 }
